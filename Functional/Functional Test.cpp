f04cd1f6e38d05f1eac180c990205489150aec96
IDragnev <iddragnev@gmail.com>
2019-07-20 18:35:44 +0300

    Add the existing code

---
+++ Functional/Functional Test.cpp
@@ -0,0 +1,220 @@
+#include "CppUnitTest.h"
+#include "Functional.h"
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <array>
+
+using namespace std::string_literals;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace IDragnev::Functional;
+
+namespace FunctionalTest
+{		
+	auto sum = [](auto x, auto y) { return x + y; };
+	auto multiply = [](auto x, auto y) { return x * y; };
+	auto toString = [](auto num) { return std::to_string(num); };
+
+	TEST_CLASS(FunctionalTest)
+	{
+	public:
+		TEST_METHOD(lessThan)
+		{
+			Assert::IsTrue(LessThan{}(1, 2));
+			Assert::IsFalse(LessThan{}(1, 1));
+			Assert::IsFalse(LessThan{}(2, 1));
+		}
+
+		TEST_METHOD(functorEqualTo)
+		{
+			Assert::IsTrue(EqualTo{}(1, 1));
+			Assert::IsFalse(EqualTo{}(1, 2));
+		}
+
+		TEST_METHOD(identity)
+		{
+			auto id = Identity{};
+
+			Assert::AreEqual(1, id(1));
+			Assert::AreNotEqual(2, id(1));
+		}
+
+		TEST_METHOD(identityUsesPerfectForwarding)
+		{
+			std::string str = "s";
+			std::string&& ref = Identity{}(std::move(str));
+
+			Assert::AreSame(ref, str);
+		}
+
+		TEST_METHOD(lessThanEqualToAndIdentityCanComputeAtCompileTime)
+		{
+			using TrueType = std::bool_constant<LessThan{}(1, 2)>;
+			using FalseType = std::bool_constant<EqualTo{}(1, 2)>;
+			using ArrayOfSizeOne = std::array<int, Identity{}(1)>;
+		}
+
+		TEST_METHOD(plusTakesLeftOperand)
+		{
+			using Strings = std::vector<std::string>;
+			auto strings = Strings{ "a", "b", "c" };
+			const auto expected = Strings{ "a!", "b!", "c!" };
+
+			std::transform(std::begin(strings), std::end(strings), std::begin(strings), plus("!"));
+
+			Assert::IsTrue(strings == expected);
+		}
+
+		TEST_METHOD(plusForwardsItsArgument)
+		{
+			auto source = "123"s;
+			
+			auto result = plus("456")(std::move(source));
+
+			Assert::IsTrue(result == "123456"s);
+			Assert::IsTrue(source == ""s);
+		}
+
+		TEST_METHOD(inversingAPredicate)
+		{
+			auto predicate = [](int x) { return x >= 0; };
+			auto nums = { 1, 2, -1, 2 };
+
+			auto it = std::find_if(std::begin(nums), std::end(nums), inverse(predicate));
+
+			Assert::IsNotNull(it);
+			Assert::AreEqual(-1, *it);
+		}
+
+		TEST_METHOD(inverseCanComputAtCompileTime)
+		{
+			constexpr auto isZero = [](auto x) constexpr { return x == 0; };
+			static_assert(inverse(isZero)(1));
+		}
+
+		TEST_METHOD(superposition)
+		{
+			auto f = [](auto x, auto y) { return x >= y; };
+			auto g = superpose(f, multiply, sum);
+			auto h = superpose(f, sum, multiply);
+
+			Assert::IsTrue(g(2, 3));
+			Assert::IsFalse(h(2, 3));
+		}
+
+		TEST_METHOD(composition)
+		{
+			auto f = compose(plus("789"s), plus("456"s), toString, Identity{});
+
+			Assert::AreEqual(f(123), "123456789"s);
+		}
+
+		TEST_METHOD(superpositionHandlesReferenceReturnTypes)
+		{
+			auto id = Identity{};
+			testHandlesReferenceReturnTypes(superpose(id, id));
+		}
+
+		TEST_METHOD(compositionHandlesReferenceReturnTypes)
+		{
+			auto id = Identity{};
+			testHandlesReferenceReturnTypes(compose(id, id));
+		}
+
+	private:
+		template <typename Callable>
+		void testHandlesReferenceReturnTypes(Callable f)
+		{
+			auto source = "123"s;
+			auto copy = source;
+
+			Assert::AreSame(source, f(source));
+			Assert::AreNotSame(source, f(copy));
+		}
+
+	public:
+		TEST_METHOD(superpositionAndCompositionCanComputeAtCompileTime)
+		{
+			constexpr auto plusOne = [](auto x) constexpr { return x + 1; };
+			constexpr auto minusOne = [](auto x) constexpr { return x - 1; };
+			constexpr auto areEqual = [](auto x, auto y) constexpr { return x == y; };
+
+			using FalseType = std::bool_constant<superpose(areEqual, plusOne, minusOne)(4)>;
+			using ArrayOfSizeOne = std::array<int, compose(minusOne, plusOne)(1)>;
+		}
+
+		TEST_METHOD(higherOrderEqualTo)
+		{
+			Assert::IsTrue(equalTo("123"s)("123"s));
+			Assert::IsFalse(equalTo("lhs"s)("rhs"s));
+		}
+
+		TEST_METHOD(higherOrderEqualToAllowsImplicitTypeConversions)
+		{
+			struct X
+			{
+				int x = 1;
+				operator int() const { return x; }
+			} x;
+
+			Assert::IsTrue(equalTo(1)(x));
+		}
+
+		TEST_METHOD(matchingByKey)
+		{
+			struct Item
+			{
+				std::string key = "target";
+			} first, second{ "s" };
+			auto extractKey = [](const auto& x) { return x.key; };
+			auto matchesTarget = matches(first.key, extractKey);
+
+			Assert::IsTrue(matchesTarget(first));
+			Assert::IsFalse(matchesTarget(second));
+		}
+
+		TEST_METHOD(curryingAFunction)
+		{
+			auto f = [](auto x, auto y, auto z) { return x + y + z; };
+			
+			auto curriedF = curry(f);		
+
+			Assert::AreEqual(curriedF(1, 2, 3), 6);
+			Assert::AreEqual(curriedF(1, 2)(3), 6);
+			Assert::AreEqual(curriedF(1)(2, 3), 6);
+			Assert::AreEqual(curriedF(1)(2)(3), 6);
+		}
+
+		TEST_METHOD(theCurriedFunctionCapturesByForwardingAndThenMovesAllTheArgumentsOnInvocation)
+		{
+			auto f = [](int&& x, std::string&& y) { return x; };
+			const auto x = 1;
+
+			auto curriedF = curry(f);
+
+			Assert::AreEqual(curriedF(x)("y"s), x);
+		}
+
+		TEST_METHOD(passingNonCopiableTypesByRefToTheCurriedFunction)
+		{
+			struct NonCopiable
+			{
+				int sum(int y, int z) const { return x + y + z; }
+
+				const int x = 10;
+			} nonCopiable;
+			
+			auto f = [](const NonCopiable& x, int y, int z) { return x.sum(y, z); };
+
+			auto curriedF = curry(f);
+			auto fWithBoundX = curriedF(std::cref(nonCopiable));
+
+			Assert::AreEqual(fWithBoundX(1, 2), 13);
+		}
+
+		TEST_METHOD(flippingABinaryFunction)
+		{
+			static_assert(flip(LessThan{})(2, 1));
+		}
+	};
+}
---
+++ Functional/Functional.h
@@ -0,0 +1,166 @@
+#ifndef __FUNCTIONAL_H_INCLUDED__
+#define __FUNCTIONAL_H_INCLUDED__
+
+#include <type_traits>
+
+namespace IDragnev::Functional
+{	
+	struct LessThan
+	{
+		template <typename T, typename U>
+		constexpr auto operator()(const T& lhs, const U& rhs) const noexcept(noexcept(lhs < rhs))
+		{
+			return lhs < rhs;
+		}
+	};
+
+	struct EqualTo
+	{
+		template <typename T, typename U>
+		constexpr auto operator()(const T& lhs, const U& rhs) const noexcept(noexcept(lhs == rhs))
+		{
+			return lhs == rhs;
+		}
+	};
+
+	struct Identity
+	{
+		template <typename T>
+		constexpr decltype(auto) operator()(T&& item) const noexcept
+		{
+			return std::forward<T>(item);
+		}
+	};
+
+	struct EmptyFunction
+	{
+		template <typename... Args>
+		constexpr void operator()(Args&&...) const noexcept { }
+	};
+
+	struct BackInserter
+	{
+		template <typename Container, typename T>
+		decltype(auto) operator()(Container& c, T&& item) const noexcept(noexcept(c.insertBack(std::forward<T>(item))))
+		{
+			return c.insertBack(std::forward<T>(item));
+		}
+	};
+
+	namespace Detail
+	{
+		template <typename... Ts>
+		inline constexpr bool areNothrowCopyConstructible = (std::is_nothrow_copy_constructible_v<Ts> && ...);
+	}
+
+	template <typename F>
+	F superpose(F&&) = delete;
+
+	template <typename F, typename... Gs>
+	constexpr inline 
+	auto superpose(F f, Gs... funs) noexcept(Detail::areNothrowCopyConstructible<F, Gs...>)
+	{
+		return [f, funs...](const auto&... args) constexpr mutable -> decltype(auto)
+		{
+			return f(funs(args...)...);
+		};
+	}
+
+	template <typename F, typename G>
+	constexpr inline 
+	auto compose(F f, G g) noexcept(noexcept(superpose(f, g)))
+	{
+		return superpose(f, g);
+	}
+
+	template <typename F, typename G, typename... Gs>
+	constexpr auto compose(F f, G g, Gs... funs) noexcept(noexcept(compose(f, g)))
+	{
+		return compose(compose(f, g), funs...);
+	}
+
+	template <typename T>
+	inline auto equalTo(T key) noexcept(std::is_nothrow_move_constructible_v<T>)
+	{
+		return [lhs = std::move(key)](const auto& rhs)
+		{ 
+			return lhs == rhs;
+		};
+	}
+
+	template <typename Key, typename KeyExtractor>
+	inline auto matches(Key key, KeyExtractor extractKey) 
+	{
+		return compose(equalTo(std::move(key)), std::move(extractKey));
+	}
+
+	template <typename T>
+	inline auto plus(T rhs) noexcept(std::is_nothrow_move_constructible_v<T>)
+	{
+		return[rhs = std::move(rhs)](auto&& lhs) -> decltype(auto)
+		{
+			using F = decltype(lhs);
+			return std::forward<F>(lhs) + rhs;
+		};
+	}
+
+	template <typename Predicate>
+	constexpr inline 
+	auto inverse(Predicate p) noexcept(std::is_nothrow_copy_constructible_v<Predicate>)
+	{
+		return [p](auto&&... args) constexpr noexcept(std::is_nothrow_invocable_v<decltype(p), decltype(args)...>)
+		{
+			return !p(std::forward<decltype(args)>(args)...);
+		};
+	}
+
+	namespace Detail
+	{
+		template <typename Callable, typename ArgsTuple>
+		inline constexpr bool isInvocableWithPackedArgs = false;
+
+		template <typename Callable, typename... Args>
+		inline constexpr bool isInvocableWithPackedArgs<Callable, std::tuple<Args...>> = std::is_invocable_v<Callable, Args...>;
+
+		template <typename Callable, typename... Args>
+		auto curryImpl(Callable f, std::tuple<Args...>&& t)
+		{
+			return [f, boundArgs = std::move(t)](auto&&... rest) -> decltype(auto)
+			{
+				auto newArgs = std::make_tuple(std::forward<decltype(rest)>(rest)...);
+				auto args = std::tuple_cat(std::move(boundArgs), std::move(newArgs));
+
+				using ArgsTuple = decltype(std::tuple_cat(std::move(boundArgs), std::move(newArgs)));
+				
+				if constexpr (isInvocableWithPackedArgs<Callable, ArgsTuple>)
+				{
+					return std::apply(f, std::move(args));
+				}
+				else
+				{
+					return Detail::curryImpl(f, std::move(args));
+				}
+			};
+		}
+	}
+
+	template <typename Callable>
+	inline auto curry(Callable f)
+	{
+		return Detail::curryImpl(f, {});
+	}
+
+	template <typename BinaryFunction>
+	constexpr auto flip(BinaryFunction f) noexcept
+	{
+		return[f](auto&& x, auto&& y) constexpr noexcept(std::is_invocable_v<decltype(f), decltype(y), decltype(x)>)
+		-> decltype(auto)
+		{
+			using X = decltype(x);
+			using Y = decltype(y);
+			return f(std::forward<Y>(y), std::forward<X>(x));
+		};
+	}
+}
+
+#endif //__FUNCTIONAL_H_INCLUDED__
